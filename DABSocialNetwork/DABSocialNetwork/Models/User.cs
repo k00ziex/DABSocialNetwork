be61b2753f11dafa062a500d96f59e6eb470e346
k00ziex <andreas_vorgaard@hotmail.com>
2019-05-13 10:14:01 +0200

    User model created

---
+++ DABSocialNetwork/DABSocialNetwork/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace DABSocialNetwork.Models
+{
+    public class User
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        // Yes, you can be an attack helicopter if you'd like
+        public string Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        
+    }
+}
