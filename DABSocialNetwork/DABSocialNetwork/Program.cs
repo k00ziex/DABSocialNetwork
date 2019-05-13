1dec4e7c1b80a663131ce8c0f656c003d5a755f5
k00ziex <andreas_vorgaard@hotmail.com>
2019-05-13 10:40:54 +0200

    Eksempel på indsættelse

--- DABSocialNetwork/DABSocialNetwork/Program.cs
+++ DABSocialNetwork/DABSocialNetwork/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using DABSocialNetwork.Models;
 
 namespace DABSocialNetwork
 {
@@ -10,6 +12,16 @@ namespace DABSocialNetwork
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting");
+            var client = new MongoDB.Driver.MongoClient();
+            var db = client.GetDatabase("DABMandatory3");
+            var UserColl = db.GetCollection<User>("User");
+            var PostColl = db.GetCollection<Post>("Post");
+            var CommentColl = db.GetCollection<Comment>("Comment");
+
+            var user = new User(){Age = 18, Email = "FakeMail", Gender = "Male", Name = "Andy"};
+            UserColl.InsertOne(user);
+            Console.WriteLine("Done");
         }
     }
 }
