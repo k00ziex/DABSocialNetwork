5664d03d673bd571c5732b65b8643792ae378bc9
ALE <holmern251@gmail.com>
2019-05-14 14:20:10 +0200

    Har ikke rigtigt lavet noget men skal pulle

--- DABSocialNetwork/DABSocialNetwork/Creation.cs
+++ DABSocialNetwork/DABSocialNetwork/Creation.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DABSocialNetwork.Models;
+using MongoDB.Bson;
 
 namespace DABSocialNetwork
 {
     public class Creation
     {
-        
+        public void CreatePost(ObjectId owner_id, Post post, Circle circle)
+        {
+            
+        }
+
+        public void CreateComment()
+        {
+
+        }
     }
 }
